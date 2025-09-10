c14da014e655fb383af99f271175d918ae303375
Bristy <tahminabristybd@gmail.com>
2025-09-10 20:19:19 +0600

    added three pages exams,knowledgeCenter,Careers

---
+++ src/components/KnoledgeCenter/KnoledgeCenter.tsx
@@ -0,0 +1,14 @@
+import CategoryList from "./CategoryList";
+import KnowledgeCenterList from "./KnowledgeCenterList";
+
+
+const KnoledgeCenter = () => {
+    return (
+        <div>
+            <CategoryList></CategoryList>
+            <KnowledgeCenterList></KnowledgeCenterList>
+        </div>
+    );
+};
+
+export default KnoledgeCenter;
