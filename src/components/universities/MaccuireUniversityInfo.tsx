5615c8baa3e2344d9db3e61bb2187b9b4a4e4094
Bristy <tahminabristybd@gmail.com>
2025-10-01 00:35:45 +0600

    part done

---
+++ src/components/universities/MaccuireUniversityInfo.tsx
@@ -0,0 +1,49 @@
+
+
+const MaccuireUniversityInfo = () => {
+     return (
+        <section className="mx-auto bg-white rounded-2xl p-10 shadow-md">
+            <h2 className="text-3xl font-bold mb-6">ম্যাককোয়ারি বিশ্ববিদ্যালয় সম্পর্কে</h2>
+            <p className="mb-4 leading-relaxed text-gray-800">
+               ম্যাককোয়ারি বিশ্ববিদ্যালয়, যা ১৯৬৪ সালে প্রতিষ্ঠিত, শিক্ষা ও গবেষণায় উচ্চাভিলাষী দৃষ্টিভঙ্গির জন্য পরিচিত একটি শীর্ষস্থানীয় প্রতিষ্ঠান হিসাবে বিবেচিত হয়েছে। সময়ের সাথে সাথে এটি একাডেমিক উৎকর্ষতা ও উপযুক্ত শিক্ষার পরিবেশ তৈরিতে দুটি খ্যাতি অর্জন করেছে।
+            </p>
+            <p className="mb-4 leading-relaxed text-gray-800">
+               মোট ভর্তি সংখ্যা: ৪৪,০০০ শিক্ষার্থী, যার মধ্যে ১২,৯৯৯ জন আন্তর্জাতিক শিক্ষার্থী বিভিন্ন দেশ থেকে এসেছে। এটি বহু-সাংস্কৃতিক পরিবেশ আপনার শিক্ষাগত অভিজ্ঞতাকে সমৃদ্ধ করে এবং আপনাকে বৈশ্বিক কর্মক্ষেত্রের জন্য প্রস্তুত করবে।
+            </p>
+            <p className="mb-4 leading-relaxed text-gray-800">
+               সিডনি, অস্ট্রেলিয়া। ক্যাম্পাসটি আপনার একাডেমিক যাত্রাকে সমর্থন করার জন্য অত্যাধুনিক সুবিধাসমূহ নিয়ে গঠিত। প্রধান সুবিধাগুলি হল:
+            </p>
+            <ul className="list-disc list-inside mb-4 text-gray-800">
+                <li>আধুনিক লেকচার হল এবং শ্রেণিকক্ষ</li>
+                <li>বিস্তৃত গ্রন্থাগার</li>
+                <li>গবেষণা কেন্দ্র ও গবেষণাগার</li>
+                <li>ক্রীড়া ও বিনোদন সুবিধা</li>
+                <li>ক্যাফে ও শিক্ষার্থী লাউঞ্জ</li>
+            </ul>
+            <p className="mb-4 leading-relaxed text-gray-800">
+                জনপ্রিয় ও শিল্পসম্মত প্রোগ্রামসমূহ:
+            </p>
+            <ul className="list-disc list-inside mb-4 text-gray-800">
+                <li>ব্যবসা ও ব্যবস্থাপনা</li>
+                <li>তথ্য প্রযুক্তি</li>
+                <li>স্বাস্থ্য বিজ্ঞান</li>
+                <li>শিক্ষা</li>
+                <li>পরিবেশগত অধ্যয়ন</li>
+            </ul>
+            <p className="mb-4 leading-relaxed text-gray-800">
+                আন্তর্জাতিক ছাত্র-ছাত্রী: ১০০+ সারা বছর ধরে একাধিক ইনটেক পিরিয়ড রয়েছে। আন্তর্জাতিক শিক্ষার্থীদের জন্য রয়েছে বিস্তৃত সহায়তা সেবা, যেমন অরিয়েন্টেশন প্রোগ্রাম, একাডেমিক পরামর্শ, এবং সাংস্কৃতিক সংহতি কার্যক্রম।
+            </p>
+            <p className="mb-4 leading-relaxed text-gray-800">
+                স্বীকৃতিদাতা সংস্থাগুলি :
+            </p>
+            <ul className="list-disc list-inside mb-4 text-gray-800">
+                <li>TEQSA (Tertiary Education Quality and Standards Agency)</li>
+                <li>AACSB (Association to Advance Collegiate Schools of Business)</li>
+            </ul>
+            <p className="mb-0 leading-relaxed text-gray-800">
+               ম্যাককোয়ারি বিশ্ববিদ্যালয়ে ভর্তি হওয়া মানে একটি মানসম্মত শিক্ষায় বিনিয়োগ করা, যা আপনাকে উচ্চতর ক্যারিয়ারের জন্য প্রয়োজনীয় দক্ষতা ও জ্ঞান সরবরাহ করবে। আপনি এখানে একটি সহায়ক কমিউনিটি— যা আপনার সাফল্যের প্রতি অংগীকারবদ্ধ।
+            </p>
+        </section>
+    );};
+
+export default MaccuireUniversityInfo;
---
+++ src/components/universities/MonashUniversityInfo.tsx
@@ -0,0 +1,49 @@
+
+
+const MonashUniversityInfo = () => {
+    return (
+    <section className="mx-auto bg-white rounded-2xl p-10 shadow-md">
+      <h2 className="text-3xl font-bold mb-6">
+       মনাশ ইউনিভার্সিটি সম্পর্কে
+      </h2>
+      <p className="mb-4 leading-relaxed text-gray-800">
+       মনাস বিশ্ববিদ্যালয়, সম্মানিত স্যার জন মনাসের নামে নামকরণ করা হয়েছে— যিনি অস্ট্রেলিয়ান সমাজের বিভিন্ন ক্ষেত্রে গুরুত্বপূর্ণ অবদান রেখেছেন। এই বিশ্ববিদ্যালয়ে ৩টি মহাদেশ জুড়ে মোট ৮টি ক্যাম্পাস রয়েছে।
+      </p>
+      <p className="mb-4 leading-relaxed text-gray-800">
+     বিশ্বব্যাপী বিস্তৃত আন্তর্জাতিক পার্টনারশিপ ও স্টাডি অ্যাব্রড প্রোগ্রামের মাধ্যমে এটি গর্বের সাথে অস্ট্রেলিয়ার সবচেয়ে বড় বিশ্ববিদ্যালয় হিসেবে পরিচিত।
+      </p>
+      <p className="mb-4 leading-relaxed text-gray-800">
+        অস্ট্রেলিয়ার ক্যাম্পাসসমূহ
+        <div className="ml-8">
+          <li>ক্লটন (প্রধান ক্যাম্পাস)</li>
+
+          <li>কওলফিল্ড</li>
+
+          <li>পেনিনসুলা</li>
+
+          <li>বেরউইক</li>
+
+          <li>গিপসল্যান্ড (বেরউইক ক্যাম্পাসসহ)</li>
+          <li>সিটি (মেলবোর্ন সিবিডি)</li>
+        </div>
+      </p>
+
+      <p className="mb-4 leading-relaxed text-gray-800">
+        আন্তর্জাতিক ক্যাম্পাসসমূহ
+        <div className="ml-8">
+          <li>মালয়েশিয়া (মনাস মালয়েশিয়া ক্যাম্পাস)</li>
+
+          <li>ইন্দোনেশিয়া</li>
+
+          
+        </div>
+      </p>
+
+     
+
+      
+    </section>
+  );
+}
+
+export default MonashUniversityInfo;
