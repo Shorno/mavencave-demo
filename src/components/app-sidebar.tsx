c18d129d4c8e0845b22a4b7495e79b54544e0714
Shorno <fb.shorno@gmail.com>
2025-10-24 21:53:20 +0600

    add admin dashboard

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,75 @@
+import * as React from "react"
+import {
+    IconChartBar,
+    IconDashboard,
+    IconFolder,
+    IconListDetails, IconRobot,
+    IconUsers,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/nav-main"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import {SettingsIcon} from "lucide-react";
+import {Link} from "react-router";
+
+const navLinks = [
+    {
+        title: "ড্যাশবোর্ড",
+        url: "/admin/dashboard",
+        icon: IconDashboard,
+    },
+    {
+        title: "ব্যবহারকারীরা",
+        url: "/admin/dashboard/users",
+        icon: IconUsers,
+    },
+    {
+        title: "ব্লগসমূহ",
+        url: "/admin/dashboard/blogs",
+        icon: IconListDetails,
+    },
+    {
+        title: "কোর্সসমূহ",
+        url: "/admin/dashboard/courses",
+        icon: IconFolder,
+    },
+    {
+        title: "বিজ্ঞাপন",
+        url: "/admin/dashboard/ads",
+        icon: IconChartBar,
+    },
+    {
+        title: "AI কুয়েরি",
+        url: "/admin/dashboard/ai-query",
+        icon: IconRobot,
+    },
+    {
+        title: "সেটিংস",
+        url: "/admin/dashboard/settings",
+        icon: SettingsIcon,
+    },
+]
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+             <Link to={"/"}>
+                 <span className={"text-2xl font-semibold text-blue-500"}>MavenCave</span>
+             </Link>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={navLinks} />
+      </SidebarContent>
+    </Sidebar>
+  )
+}
