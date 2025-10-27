1f3d1d7393305f0fc1c6d0d13ffd87404383b06b
Bristy <tahminabristybd@gmail.com>
2025-10-27 20:36:27 +0600

    build error fixed

--- src/components/DashBoardOverviewComponents/CountryPieChart.tsx
+++ src/components/DashBoardOverviewComponents/CountryPieChart.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
 import type { CountryData } from "../../types/DashBoardTypes";
-import { Legend, Pie, PieChart } from "recharts";
+import { Cell, Legend, Pie, PieChart } from "recharts";
 import { Tooltip } from "../ui/tooltip";
-import { TooltipContent } from "@radix-ui/react-tooltip";
+
 
 interface Props {
   countries: CountryData[];
@@ -20,21 +20,21 @@ const CountriesPieChart: React.FC<Props> = ({ countries }) => {
 
       <div className="flex justify-center items-center h-[250px]">
         <PieChart>
-          <Tooltip content={<TooltipContent />} />
-          <Legend className="text-sm" />
           <Pie
             data={countries}
             dataKey="value"
             nameKey="name"
             innerRadius={40}
             outerRadius={80}
-            fill="#7C3AED"
             stroke="white"
           >
             {countries.map((_, i) => (
-              <cell key={i} fill={COLORS[i % COLORS.length]} />
+              <Cell key={i} fill={COLORS[i % COLORS.length]} />
             ))}
           </Pie>
+
+          <Tooltip />
+          <Legend className="text-sm" />
         </PieChart>
       </div>
     </div>
